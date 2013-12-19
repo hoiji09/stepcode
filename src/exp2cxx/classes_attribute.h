900d01317b0003ea0fcbc5d8349cf497e8182210
Mark Pictor <mpictor@gmail.com>
2013-12-19 18:36:21 -0500

    split entity methods out of ATTRprint_access_methods into 2 new funcs
    second new func prints log stuff, called by other func 3x

--- src/exp2cxx/classes_attribute.h
+++ src/exp2cxx/classes_attribute.h
@@ -7,4 +7,16 @@ char * generate_attribute_func_name( Variable a, char * out );
 void ATTRsign_access_methods( Variable a, const char * objtype, FILE * file );
 void ATTRprint_access_methods( CONST char * entnm, Variable a, FILE * file );
 
+/** print logging code for access methods for attrs that are entities
+ * \p var is the variable name, minus preceding underscore, or null if 'x' is to be used
+ * \p dir is either "returned" or "assigned"
+ */
+void ATTRprint_access_methods_entity_logging( const char * entnm, const char * funcnm, const char * nm,
+                                              const char * var, const char * dir, FILE * file );
+
+/** print access methods for attrs that are entities
+ * prints const and non-const getters and a setter
+ */
+void ATTRprint_access_methods_entity( const char * entnm, const char * attrnm, const char * funcnm, const char * nm,
+                                      const char * ctype, Variable a, FILE * file );
 #endif
